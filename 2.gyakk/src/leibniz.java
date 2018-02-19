cdb5e97d3f7dffd9724d6bae470baecddd563ba7
ladi <ladi@lab104-24.iit.uni-miskolc.hu>
2018-02-19 16:17:37 +0100

    Wallis-formula implementalva

---
+++ 2.gyakk/src/leibniz.java
@@ -0,0 +1,10 @@
+
+public class leibniz {
+public static void main(String[] args) {
+		double pi = 1
+		int darab = 1000 / 2;
+				for (int i = 1; i <= darab; i++) {
+					
+				}
+	}
+}
---
+++ 2.gyakk/src/piertek.java
@@ -0,0 +1,19 @@
+
+public class piertek {
+	public static void main(String[] args) {
+		//Wallis-formula
+		double pi = 1;
+		int darab = 100000000 / 2;
+		
+		for (int i = 1; i <= darab; i++) {
+			pi *= 2.0 * i / (2 * i - 1);
+			pi *= 2.0 * i / (2 * i + 1);
+		}
+		System.out.println("Wallis-formula");
+		
+		System.out.println("pi/2 erteke: " + pi);
+		System.out.println("pi/2 erteke: " + pi * 2);
+
+	}
+	
+}
