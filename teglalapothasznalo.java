a0cff8995ea4f5c8a8f3451e98282074d2ae9fe3
Andras Ladi <36538403+ladiand@users.noreply.github.com>
2018-04-09 15:12:20 +0200

    Add files via upload
    5. gyakorlat

---
+++ teglalapothasznalo.java
@@ -0,0 +1,30 @@
+
+public class teglalapothasznalo {
+	public static void main(String[] args) {
+		teglalap a_objektum = new teglalap(2);
+		teglalap b_objektum = new teglalap(3, 4);
+		teglalap c_objektum;
+		c_objektum = a_objektum;
+		
+		System.out.println(a_objektum);
+		System.out.println(b_objektum);
+		System.out.println(c_objektum);
+		
+		a_objektum.setOldalak(5);
+		
+		System.out.println(a_objektum);
+		System.out.println(b_objektum);
+		System.out.println(c_objektum);
+		
+		a_objektum.setOldalak(3, 4);
+		
+		System.out.println(a_objektum);
+		System.out.println(b_objektum);
+		System.out.println(c_objektum);
+	
+		System.out.println(a_objektum == b_objektum);
+		System.out.println(a_objektum == c_objektum);
+		System.out.println(a_objektum.egyezikE(b_objektum));
+		
+	}
+}
